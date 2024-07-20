483394e3c3148e753627c86efc0456cebb85ffaa
Karina Sodré <karinasodre.ts@gmail.com>
2024-07-19 21:47:12 -0300

    Adiciona projeto jogoDaVelha

---
+++ Program/src/Campo.java
@@ -0,0 +1,16 @@
+public class Campo {
+    private char simbolo;
+    public Campo() {
+        this.simbolo=' ';
+    }
+    public char getSimbolo() {
+        return this.simbolo;
+    }
+    public void setSimbolo(char simbolo) {
+        if (this.simbolo == ' ') {
+            this.simbolo = simbolo;
+        } else {
+            System.out.print("Campo ja usado");
+        }
+    }
+}
